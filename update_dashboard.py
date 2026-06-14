955eaae2f573b8da8f07000a093c1d0c11860070
arun.vishnu <arun.vishnu@gmail.com>
2026-06-14 05:37:30 -0400

    add update_dashboard.py deploy script

---
+++ update_dashboard.py
@@ -0,0 +1,24 @@
+import urllib.request, zipfile, shutil, os
+
+url = "https://github.com/arunvishnu/akami-ha-dashboard/archive/refs/heads/deploy.zip"
+tmp_zip = "/tmp/da.zip"
+tmp_dir = "/tmp/da"
+dest = "/config/www/ha-dashboard"
+
+print("Downloading...")
+urllib.request.urlretrieve(url, tmp_zip)
+
+print("Extracting...")
+if os.path.exists(tmp_dir):
+    shutil.rmtree(tmp_dir)
+with zipfile.ZipFile(tmp_zip) as z:
+    z.extractall(tmp_dir)
+
+extracted = os.path.join(tmp_dir, os.listdir(tmp_dir)[0], "www", "ha-dashboard")
+if os.path.exists(dest):
+    shutil.rmtree(dest)
+shutil.copytree(extracted, dest)
+
+shutil.rmtree(tmp_dir)
+os.remove(tmp_zip)
+print("Done")
